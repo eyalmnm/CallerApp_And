bffb6e355f4a43c647aa0b59b83c704a155963b3
eyalmnm <eyal@em-projects.com>
2017-07-20 23:12:46 +0300

    This commit contains:  - Add failures tracker.

--- app/src/main/java/com/em_projects/callerapp/MainActivity.java
+++ app/src/main/java/com/em_projects/callerapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.em_projects.callerapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.em_projects.callerapp.tracer.ExceptionHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExceptionHandler.register(this);
     }
 }
