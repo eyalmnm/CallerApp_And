f1e1e432b253c0c145e189e46335b9ae7236845c
eyalmnm <eyal@em-projects.com>
2017-07-25 22:12:09 +0300

    This commit contains:  - Add Boot and Shutdown Receivers. Not Completed!!!

---
+++ app/src/main/java/com/em_projects/callerapp/boot/OnBootReceiver.java
@@ -0,0 +1,19 @@
+package com.em_projects.callerapp.boot;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by eyalmuchtar on 25/07/2017.
+ */
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Reload the application
+        Intent startIntent = new Intent(context, SpalshActivity.class);
+        context.startActivity(startIntent);
+    }
+}
---
+++ app/src/main/java/com/em_projects/callerapp/boot/ShutDownReceiver.java
@@ -0,0 +1,17 @@
+package com.em_projects.callerapp.boot;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by eyalmuchtar on 25/07/2017.
+ */
+
+public class ShutDownReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Do Something
+    }
+}
