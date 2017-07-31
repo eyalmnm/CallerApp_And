f163d8d483b6df9a7c04dfbb24e5810b136e7aee
eyalmnm <eyal@em-projects.com>
2017-07-31 21:28:35 +0300

    This commit contains:  - Add Telephony data layer.

--- app/src/main/java/com/em_projects/callerapp/config/Constants.java
+++ app/src/main/java/com/em_projects/callerapp/config/Constants.java
@@ -7,4 +7,15 @@ package com.em_projects.callerapp.config;
 public class Constants {
 
     public static final String shared_preferences_name = "caller_app_pref";
+
+    // Radio - Telephony Utils
+    public static String mcc = "mcc";
+    public static String mnc = "mnc";
+    public static String lac = "lac";
+    public static String cid = "cid";
+    public static String psc = "psd";
+    public static String radio = "radio";
+    public static String dbm = "dbm";
+    public static String lvl = "lvl";
+    public static String cell_identity = "cell_identity";
 }
