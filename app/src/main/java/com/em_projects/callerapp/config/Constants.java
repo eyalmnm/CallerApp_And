0e398c39502fde4744592ca2d3d29fd10d530034
eyalmnm <eyal@em-projects.com>
2017-08-10 23:46:53 +0300

    This commit contains:  - Add OTP properties.

--- app/src/main/java/com/em_projects/callerapp/config/Constants.java
+++ app/src/main/java/com/em_projects/callerapp/config/Constants.java
@@ -18,4 +18,38 @@ public class Constants {
     public static String dbm = "dbm";
     public static String lvl = "lvl";
     public static String cell_identity = "cell_identity";
+
+
+    // Server Communication parameters
+    public static String serverURL;
+    public static String ourSecret;
+    public static String deviceId;
+    public static String phoneNumber;
+    public static String timeStamp;
+    public static String otp;
+
+    // Server Communication methods
+    public static String smsVerification;
+    public static String otpVerification;
+
+
+    // Server Communication secret
+    public static String secret = "sfasfwtweerwenjhvytetqw8er9y8rhhjvctu67r687et9yqiehnnbebfdq8etqo";
+
+    // Connection token
+    public static String token = "token";
+
+
+
+    // SMS Verifcation Section
+    //*****************************
+    // SMS provider identification
+    // It should match with your SMS gateway origin
+    // You can use  MSGIND, TESTER and ALERTS as sender ID
+    // If you want custom sender Id, approve MSG91 to get one
+    public static final String SMS_ORIGIN = "Cellcom";
+
+    // special character to prefix the otp. Make sure this character appears only once in the sms
+    public static final String OTP_DELIMITER = ":";
+
 }
--- app/src/main/java/com/em_projects/callerapp/config/Dynamic.java
+++ app/src/main/java/com/em_projects/callerapp/config/Dynamic.java
@@ -1,10 +1,45 @@
 package com.em_projects.callerapp.config;
 
+import android.content.Context;
+
+import com.em_projects.callerapp.utils.PreferencesUtils;
+
 /**
  * Created by eyal muchtar on 20/07/2017.
  */
 
 public class Dynamic {
-    public static String myNumber;
-    public static String call_unique_ID;
+
+    private static String myNumber;
+    private static String myToken;
+    private static String call_unique_ID;
+
+    public Dynamic(Context context) {
+        myNumber = PreferencesUtils.getInstance(context).getPhone();
+        myToken = PreferencesUtils.getInstance(context).getToken();
+    }
+
+    public static String getMyNumber() {
+        return myNumber;
+    }
+
+    public static String getMyToken() {
+        return myToken;
+    }
+
+    public static void setCall_unique_ID(String call_unique_ID) {
+        Dynamic.call_unique_ID = call_unique_ID;
+    }
+
+    public static String getCall_unique_ID() {
+        return call_unique_ID;
+    }
+
+    public static void setMyNumber(String myNumber) {
+        Dynamic.myNumber = myNumber;
+    }
+
+    public static void setMyToken(String myToken) {
+        Dynamic.myToken = myToken;
+    }
 }
