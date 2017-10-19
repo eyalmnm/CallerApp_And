01911575207a5664df78c35aaf5f23a2516840c7
eyalmnm <eyal@em-projects.com>
2017-10-19 11:19:41 +0300

    This commit contains:  - Add getContacts method.

--- app/src/main/java/com/em_projects/callerapp/utils/ContactsUtils.java
+++ app/src/main/java/com/em_projects/callerapp/utils/ContactsUtils.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by eyalmuchtar on 02/01/2017.
@@ -117,21 +118,15 @@ public class ContactsUtils {
 
         if (cur.getCount() > 0) {
             while (cur.moveToNext()) {
-                String id = cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cur.getString(cur.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME));
-
-                if (cur.getInt(cur.getColumnIndex(
-                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-                    Cursor pCur = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                             null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{id}, null);
                     while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                         Toast.makeText(context, "Name: " + name
                                 + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
                     }
@@ -142,21 +137,87 @@ public class ContactsUtils {
     }
 
     public static void getContactId(Context context, String phoneNumber) {
-        Uri lookupUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,Uri.encode(phoneNumber));
+        Uri lookupUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
         String[] proj = new String[]
                 {///as we need only this colum from a table...
                         ContactsContract.Contacts.DISPLAY_NAME,
                         ContactsContract.Contacts._ID,
                 };
-        String id=null;
+        String id = null;
         String sortOrder1 = ContactsContract.CommonDataKinds.StructuredPostal.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
-        Cursor crsr = context.getContentResolver().query(lookupUri,proj, null, null, sortOrder1);
-        while(crsr.moveToNext())
-        {
-            String name=crsr.getString(crsr.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        Cursor crsr = context.getContentResolver().query(lookupUri, proj, null, null, sortOrder1);
+        while (crsr.moveToNext()) {
+            String name = crsr.getString(crsr.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
             id = crsr.getString(crsr.getColumnIndex(ContactsContract.Contacts._ID));
-            Toast.makeText(context,"Name of this cntct is   : " +name +"\n id is : "+id ,Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Name of this cntct is   : " + name + "\n id is : " + id, Toast.LENGTH_SHORT).show();
         }
         crsr.close();
+    }
+
+    public static ArrayList<String> getContacts(Context context) {
+        Cursor cursor;
+        ArrayList<String> contactList;
+        contactList = new ArrayList<String>();
+        int counter;
+
+        String phoneNumber = null;
+        String email = null;
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        Uri EmailCONTENT_URI = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+        String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+        String DATA = ContactsContract.CommonDataKinds.Email.DATA;
+
+        StringBuffer output;
+
+        ContentResolver contentResolver = context.getContentResolver();
+
+        cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
+
+        // Iterate every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            counter = 0;
+            while (cursor.moveToNext()) {
+                output = new StringBuffer();
+
+                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+
+                if (hasPhoneNumber > 0) {
+                    output.append("\n First Name:" + name);
+
+                    //This is to read multiple phone numbers associated with the same contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[]{contact_id}, null);
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        output.append("\n Phone number:" + phoneNumber);
+                    }
+                    phoneCursor.close();
+
+                    // Read every email id associated with the contact
+                    Cursor emailCursor = contentResolver.query(EmailCONTENT_URI, null, EmailCONTACT_ID + " = ?", new String[]{contact_id}, null);
+                    while (emailCursor.moveToNext()) {
+                        email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
+                        output.append("\n Email:" + email);
+                    }
+                    emailCursor.close();
+                }
+
+                // Add the contact to the ArrayList
+                contactList.add(output.toString());
+            }
+        }
+        return contactList;
     }
 }
