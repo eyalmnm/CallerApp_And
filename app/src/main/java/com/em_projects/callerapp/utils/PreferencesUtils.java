64dd7312cf7d9e5611ac4b54d8823a5b47ddc49d
eyalmnm <eyal@em-projects.com>
2017-10-19 12:44:21 +0300

    This commit contains:  - United all SharedPrefernces methods into one class.

--- app/src/main/java/com/em_projects/callerapp/utils/PreferencesUtils.java
+++ app/src/main/java/com/em_projects/callerapp/utils/PreferencesUtils.java
@@ -5,38 +5,39 @@ import android.content.SharedPreferences;
 
 import com.em_projects.callerapp.config.Constants;
 
+import static android.content.Context.MODE_PRIVATE;
+
 /**
  * Created by eyalmuchtar on 06/08/2017.
  */
 
+// @Ref: http://stackoverflow.com/questions/30719047/android-m-check-runtime-permission-how-to-determine-if-the-user-checked-nev
+// @Ref: http://stackoverflow.com/a/40639277
+
 public class PreferencesUtils {
     private static final String TAG = "PreferencesUtils";
-
+    // Shared preferences file name
+    private static final String PREF_NAME = "callApp";
+    private static PreferencesUtils instance = null;
     // Shared preferences access components
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
-
     // Application context
     private Context context;
-
-    // Shared preferences file name
-    private static final String PREF_NAME = "callApp";
     // Shared preferences working mode
     private int PRIVATE_MODE = 0;
 
-    private static PreferencesUtils instance = null;
+    private PreferencesUtils(Context context) {
+        this.context = context;
+        preferences = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = preferences.edit();
+    }
 
     public static PreferencesUtils getInstance(Context context) {
         if (null == instance) {
             instance = new PreferencesUtils(context);
         }
         return instance;
-    }
-
-    private PreferencesUtils(Context context) {
-        this.context = context;
-        preferences = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        editor = preferences.edit();
     }
 
     public void setPhoneNumber(String phoneNumber) throws Exception{
@@ -48,6 +49,24 @@ public class PreferencesUtils {
         }
     }
 
+    public boolean isRegisteredUser() {
+        String registrationCode = context.getSharedPreferences(Constants.shared_preferences_name, MODE_PRIVATE).getString("registrationCode", null);
+        return !StringUtils.isNullOrEmpty(registrationCode);
+    }
+
+    public void registerUser(String registrationCode) {
+        SharedPreferences sharedPreference = context.getSharedPreferences(Constants.shared_preferences_name, MODE_PRIVATE);
+        sharedPreference.edit().putString("registrationCode", registrationCode).apply();
+    }
+
+    public String getPhone() {
+        return preferences.getString(Constants.phoneNumber, null);
+    }
+
+    public String getToken() {
+        return preferences.getString(Constants.token, null);
+    }
+
     public void setToken(String token) throws Exception {
         if (true == StringUtils.isNullOrEmpty(token)) {
             editor.putString(Constants.token, token);
@@ -57,12 +76,17 @@ public class PreferencesUtils {
         }
     }
 
-    public String getPhone() {
-        return preferences.getString(Constants.phoneNumber, null);
+    public String getOTP() {
+        return preferences.getString(Constants.otp, null);
     }
 
-    public String getToken() {
-        return preferences.getString(Constants.token, null);
+    public void setOTP(String otp) throws Exception {
+        if (true == StringUtils.isNullOrEmpty(otp)) {
+            editor.putString(Constants.otp, otp);
+            editor.commit();
+        } else {
+            throw new Exception("Invalid Token");
+        }
     }
 
     @Override
