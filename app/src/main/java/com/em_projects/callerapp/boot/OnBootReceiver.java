58cb92f72084801a9a8b6d742099f15c5c126353
eyalmnm <eyal@em-projects.com>
2017-12-12 02:33:17 +0200

    This commit contains:  - Fix the reference to MainActivity.

--- app/src/main/java/com/em_projects/callerapp/boot/OnBootReceiver.java
+++ app/src/main/java/com/em_projects/callerapp/boot/OnBootReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.em_projects.callerapp.MainActivity;
+
 /**
  * Created by eyalmuchtar on 25/07/2017.
  */
@@ -13,7 +15,7 @@ public class OnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // Reload the application
-        Intent startIntent = new Intent(context, SplashActivity.class);
+        Intent startIntent = new Intent(context, MainActivity.class);
         context.startActivity(startIntent);
     }
 }
