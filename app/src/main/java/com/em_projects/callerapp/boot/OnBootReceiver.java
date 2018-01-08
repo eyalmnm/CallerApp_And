7306f940cc8f0c1e4a6121d51dba15689146c592
eyalmnm <eyal@em-projects.com>
2018-01-08 13:24:47 +0200

    This commit contains:  - Change the boot receiver to start the MainScreenActivity instead of MainActivity.

--- app/src/main/java/com/em_projects/callerapp/boot/OnBootReceiver.java
+++ app/src/main/java/com/em_projects/callerapp/boot/OnBootReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.em_projects.callerapp.MainActivity;
+import com.em_projects.callerapp.main.MainScreenActivity;
 
 /**
  * Created by eyalmuchtar on 25/07/2017.
@@ -15,7 +15,7 @@ public class OnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // Reload the application
-        Intent startIntent = new Intent(context, MainActivity.class);
+        Intent startIntent = new Intent(context, MainScreenActivity.class);
         startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(startIntent);
     }
