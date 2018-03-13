2ff1656bdab9f10abbe01464341f7de3f66c51eb
eyalmnm <eyal@em-projects.com>
2018-03-13 21:56:04 +0200

    This commit contains:   - Add Room Persistent.

--- app/src/main/java/com/em_projects/callerapp/storage/room/AppDatabase.java
+++ app/src/main/java/com/em_projects/callerapp/storage/room/AppDatabase.java
@@ -9,15 +9,13 @@ import android.content.Context;
  * Created by eyalmuchtar on 3/11/18.
  */
 
-@Database(entities = {CallLogDbEntety.class}, version = 1)
+@Database(entities = {CallLogDbEntety.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
-
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
-            INSTANCE =
-                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "wize_caller-database")
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "wize_caller-database")
                             // allow queries on the main thread.
                             // Don't do this on a real app! See PersistenceBasicSample for an example.
                             .allowMainThreadQueries()
